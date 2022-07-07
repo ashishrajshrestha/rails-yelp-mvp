0c7d97f04cf1d54bbcbbc3ba8c1b8835a2e98900
ashish shrestha <ashishrajshrestha1@gmail.com>
2022-07-07 21:02:46 +0900

    Rake part all completed

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,33 @@
+class ReviewsController < ApplicationController
+  def index
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    # @reviews = Review.all
+    @reviews = Review.where(restaurant_id: @restaurant)
+  end
+
+
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+
+     if @review.save
+        redirect_to restaurant_path(@review.restaurant)
+      else
+        render 'new'
+      end
+  end
+
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+
+end
